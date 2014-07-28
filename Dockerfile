357ef210d3123b2e592d41fd3ca483cbb81f6620
Rafał Rzepecki <divided.mind@gmail.com>
2014-07-28 18:53:57 -0400

    Conjur configuration created

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,5 @@ MAINTAINER Rafal Rzepecki <rafal@conjur.net>
 ADD https://s3.amazonaws.com/conjur-releases/omnibus/conjur_4.10.1-2_amd64.deb /tmp/conjur.deb
 RUN dpkg -i /tmp/conjur.deb
 RUN rm /tmp/conjur.deb
+
+COPY conjur.profile /etc/profile.d/
---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ARGS="APPLIANCE_URL CERT ACCOUNT LOGIN PASSWORD"
+
+for a in $ARGS; do
+  VAR=CONJUR_$a
+  if eval [ -z \"\$$VAR\" ]; then
+    echo "$VAR environment variable required but not set."
+    exit 1
+  fi
+done
+
+echo "$CONJUR_CERT" > /etc/conjur.pem
+
+# set up Conjur configuration
+cat > /etc/conjur.conf << EOF
+appliance_url: $CONJUR_APPLIANCE_URL
+account: $CONJUR_ACCOUNT
+netrc_path: /etc/conjur.identity
+cert_file: /etc/conjur.pem
+EOF
+
+cat > /etc/conjur.identity << EOF
+machine $CONJUR_APPLIANCE_URL/authn
+login $CONJUR_LOGIN
+password $CONJUR_PASSWORD
+EOF
+
+chmod 0600 /etc/conjur.identity
+
+bash
