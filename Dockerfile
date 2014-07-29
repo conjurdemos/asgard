804ad94aa969dc0036c7e40559ab83cfedc49602
Rafał Rzepecki <divided.mind@gmail.com>
2014-07-29 18:06:40 -0400

    Asgard launches with AWS creds from Conjur

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,7 @@ RUN rm /tmp/conjur.deb
 COPY conjur.profile /etc/profile.d/conjur.sh
 COPY conjur-configure.sh /opt/conjur/configure
 COPY asgard-run.sh /usr/sbin/launch-asgard
+COPY asgard.env.yaml /etc/asgard/
+COPY asgard.conf.erb /etc/asgard/
 
 CMD /usr/sbin/launch-asgard
--- asgard-run.sh
+++ asgard-run.sh
@@ -2,3 +2,9 @@
 
 /opt/conjur/configure
 
+CNF=`/opt/conjur/bin/conjur env template -c /etc/asgard/asgard.env.yaml /etc/asgard/asgard.conf.erb`
+mv $CNF /etc/asgard/asgard.conf
+mkdir -p /root/.asgard
+ln -sf /etc/asgard/asgard.conf /root/.asgard/Config.groovy
+
+java -Xmx1024M -XX:MaxPermSize=128m -jar /opt/asgard/asgard-standalone.jar
