73e4faf336990b83fd86cba50e1022d5dc3b83e5
Kevin Gilpin <kgilpin@gmail.com>
2015-06-08 16:00:49 -0400

    more WIP-y with docker-compose

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 docker run -v $PWD/asgard/sdf.yml:/sdf.yml sdf-gen global sdf.yml > asgard/gatekeeper/global.conf
 docker run -v $PWD/asgard/sdf.yml:/sdf.yml sdf-gen global sdf.yml > asgard/eureka/global.conf
 docker run -v $PWD/asgard/sdf.yml:/sdf.yml sdf-gen gatekeeper sdf.yml > asgard/gatekeeper/gate.conf
-docker run -v $PWD/asgard/sdf.yml:/sdf.yml sdf-gen forwarder -l host/asgard-01 -k the-api-key sdf.yml > asgard/eureka/forward.conf
+docker run -v $PWD/asgard/sdf.yml:/sdf.yml sdf-gen forwarder -l @host_id@ -k @api-key@ sdf.yml > asgard/eureka/forward.conf
 
 docker build -t asgard-service asgard/service
 docker build -t asgard-eureka asgard/eureka
